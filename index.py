8b4854528a8caee65f892b816711de4f20d55a6d
klym83977 <klym83977@gmail.com>
2026-09-17 08:15:51 +0300

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,6 @@ import telebot
 from config import YOUR_TELEGRAM_CHAT_ID
 from handlers import bot
 from notion import get_todays_tasks
-from .deye_api import get_test_connection
 
 app = Flask(__name__)
 application = app
